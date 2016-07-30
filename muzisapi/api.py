ea918847f550238ed3105102ab258d9a14bafa26
BlackPuma <Levsha810@gmail.com>
2016-07-30 19:55:16 +0300

    Добавил апи мьюзиса

---
+++ muzisapi/api.py
@@ -0,0 +1,16 @@
+import requests
+
+base_url = 'http://muzis.ru/api/{method}'
+
+url_stream_from_values = 'stream_from_values.api'
+url_stream_from_lyrics = 'stream_from_lyrics.api'
+
+
+def stream_from_values(data):
+    url = base_url.format(method=url_stream_from_values)
+    return requests.post(url, data).json()['songs']
+
+
+def stream_from_lyrics(data):
+    url = base_url.format(method=url_stream_from_lyrics)
+    return requests.post(url, data).json()['songs']
