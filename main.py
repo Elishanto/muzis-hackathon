eeffb79b46be945e5357b608f19bdc05a8a35f71
elishanto <elishanto@gmail.com>
2016-07-30 22:02:37 +0300

    1/2 working code

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import yaml
 import os
 
 from pymongo import MongoClient
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
 from handlers import Handlers
 
@@ -30,6 +30,7 @@ def main():
 
     dp.add_handler(CommandHandler('start', handlers.start_handler))
     dp.add_handler(CommandHandler('create', handlers.create_handler, pass_args=True))
+    dp.add_handler(CallbackQueryHandler(handlers.callback_handler))
 
     dp.add_error_handler(error)
 
