432e1044c13747cfd5890bc636325ea54d7e4265
elishanto <elishanto@gmail.com>
2016-07-31 04:13:42 +0300

    Refactoring

--- utils/api_utils.py
+++ utils/api_utils.py
@@ -19,8 +19,7 @@ def concatenate_audio(songs_list, params=None):
     song_names = [song['file_mp3'] for song in songs_list]
     sounds = []
     for file_name in song_names:
-        local_filename, _headers = urllib.request.urlretrieve(audio_url.format(name=file_name),
-                                                              filename=file_name)
+        local_filename, _headers = urllib.request.urlretrieve(audio_url.format(name=file_name), filename=file_name)
         sounds.append(pydub.AudioSegment.from_mp3(local_filename))
         os.remove(local_filename)
     playlist = pydub.AudioSegment.empty()
