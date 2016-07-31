bcd9c696b177ef7c8acbed6ba3ae8c52aea9a2f7
elishanto <elishanto@gmail.com>
2016-07-31 03:56:51 +0300

    Add a lot of things!

--- handlers.py
+++ handlers.py
@@ -1,19 +1,12 @@
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from utils.api_utils import generate_audio
 
-BUTTONS = [
-    [InlineKeyboardButton(text='Длительность плейлиста', callback_data='l')],
-    [InlineKeyboardButton(text='Жанры', callback_data='g')],
-    [InlineKeyboardButton(text='Музыкальная эпоха', callback_data='e')],
-    [InlineKeyboardButton(text='Тематика', callback_data='t')],
-    [InlineKeyboardButton(text='=Сохранить=', callback_data='s')]
-]
-
 
 class Handlers:
     def __init__(self, mongo, config):
         self.db = mongo.parter
         self.config = config
+        self.BUTTONS = [[InlineKeyboardButton(text=x[1], callback_data=x[0])] for x in self.config['names']]
 
     def start_handler(self, bot, update):
         return bot.sendMessage(update.message.from_user.id, text=self.config['start_message'])
@@ -24,14 +17,14 @@ class Handlers:
         else:
             name = str(self.db.playlists.count() + 1)
 
-        if self.db.playlists.find({'name': name}):
-            name = '"{}" #{}'.format(name, self.db.playlists.count({'name': name}) + 1)
+        if self.db.playlists.find({'name': name}).count() > 0:
+            name = '{} #{}'.format(name, self.db.playlists.count({'name': name}) + 1)
 
         self.db.playlists.insert_one({'name': name, 'user_id': update.message.from_user.id})
         self.db.current.update_one({'user_id': update.message.from_user.id}, {'$set': {'name': name}}, upsert=True)
 
-        message = bot.sendMessage(update.message.from_user.id, text='Playlist {}'.format(name),
-                                  reply_markup=InlineKeyboardMarkup(BUTTONS))
+        message = bot.sendMessage(update.message.from_user.id, text='Playlist "{}"'.format(name),
+                                  reply_markup=InlineKeyboardMarkup(self.BUTTONS))
         self.db.users.update_one(
             {'user_id': update.message.from_user.id},
             {'$set': {'message_id': message.message_id}},
@@ -39,6 +32,9 @@ class Handlers:
         )
 
     def callback_handler(self, bot, update):
+        def generate_desc(name):
+            text = 'Playlist "{}"'.format(name)
+            return text
         query = update.callback_query
         data = query.data
         name = self.db.current.find_one({'user_id': query.from_user.id})['name']
@@ -56,17 +52,20 @@ class Handlers:
                                        text=text)
         if len(data.split('|')) > 1:
             data = data.split('|')
-            self.db.playlists.update_one({'name': name}, {'$set': {data[0]: self.config['buttons'][data[0]][data[1]]}})
+            to_set = self.config['buttons'][data[0]][data[1]]
+            self.db.playlists.update_one({'name': name}, {'$set': {data[0]: to_set}})
             return bot.editMessageText(chat_id=query.message.chat.id,
                                        message_id=message_id,
-                                       text='Playlist "{}"'.format(name),
-                                       reply_markup=InlineKeyboardMarkup(BUTTONS))
+                                       text=generate_desc(name),
+                                       reply_markup=InlineKeyboardMarkup(self.BUTTONS))
         else:
             buttons = self.config['buttons'][data]
+
+            buttons = sorted(buttons.keys())
 
             buttons = [[InlineKeyboardButton(text=x, callback_data='{}|{}'.format(data, x))]
                        for x in buttons]
             return bot.editMessageText(chat_id=query.message.chat.id,
                                        message_id=message_id,
-                                       text='Playlist "{}"'.format(name),
+                                       text=generate_desc(name),
                                        reply_markup=InlineKeyboardMarkup(buttons))
